5685ba75f56d74e37608568f35ba7b0ebe9d2970
Alex <alejandro.ruiz@iesnervion.es>
2021-10-04 19:40:03 +0200

    Activity Act 1 y 2  creadas

---
+++ Boletin 3.2/app/src/main/java/es/iesnervion/aruiz/boletin32/Actividad1Activity.java
@@ -0,0 +1,14 @@
+package es.iesnervion.aruiz.boletin32;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actividad1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad1);
+    }
+}
