8f61f6a61853c4aef05a6a53c8e148f328c7a866
Alex <alejandro.ruiz@iesnervion.es>
2021-10-05 10:47:55 +0200

    Actividad 3.2 Numero 1 y 2

--- Boletin 3.2/app/src/main/java/es/iesnervion/aruiz/boletin32/Actividad1Activity.java
+++ Boletin 3.2/app/src/main/java/es/iesnervion/aruiz/boletin32/Actividad1Activity.java
@@ -2,13 +2,72 @@ package es.iesnervion.aruiz.boletin32;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class Actividad1Activity extends AppCompatActivity {
+public class Actividad1Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private CheckBox checkBoxNegrita;
+    private CheckBox checkBoxAumentoFuente;
+    private CheckBox checkBoxDisminucionFuente;
+    private CheckBox checkBoxColorRojo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad1);
+
+        checkBoxNegrita = findViewById(R.id.checkBoxTextBlack);
+        checkBoxAumentoFuente = findViewById(R.id.checkBoxMoreFont);
+        checkBoxDisminucionFuente = findViewById(R.id.checkBoxLessFont);
+        checkBoxColorRojo = findViewById(R.id.checkBoxColorRed);
+
+        checkBoxNegrita.setOnClickListener(this);
+        checkBoxAumentoFuente.setOnClickListener(this);
+        checkBoxDisminucionFuente.setOnClickListener(this);
+        checkBoxColorRojo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView textView = findViewById(R.id.textViewAct1);
+        switch (view.getId()){
+
+            case R.id.checkBoxTextBlack:
+                if (checkBoxNegrita.isChecked()){
+                    textView.setTypeface(null, Typeface.BOLD);
+                }else{
+                    textView.setTypeface(Typeface.DEFAULT);
+                }
+            break;
+
+            case R.id.checkBoxMoreFont:
+                if (checkBoxAumentoFuente.isChecked()){
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,50);
+                }else{
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,28);
+                }
+            break;
+            case R.id.checkBoxLessFont:
+                if (checkBoxDisminucionFuente.isChecked()){
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,5);
+                }else{
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,28);
+                }
+            break;
+
+            case R.id.checkBoxColorRed:
+                if (checkBoxColorRojo.isChecked()){
+                    textView.setTextColor(getResources().getColor(R.color.red));
+                }else{
+                    textView.setTextColor(getResources().getColor(R.color.colorTexViewDefault));
+                }
+            break;
+        }
     }
 }
--- Boletin 3.2/app/src/main/java/es/iesnervion/aruiz/boletin32/Actividad2Activity.java
+++ Boletin 3.2/app/src/main/java/es/iesnervion/aruiz/boletin32/Actividad2Activity.java
@@ -3,12 +3,69 @@ package es.iesnervion.aruiz.boletin32;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class Actividad2Activity extends AppCompatActivity {
+public class Actividad2Activity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad2);
+
+        RadioButton radioButtonSuma = findViewById(R.id.radioButtonSumar),
+                    radioButtonRestar = findViewById(R.id.radioButtonRestar),
+                    radioButtonMultiplicar = findViewById(R.id.radioButtonMultiplicar),
+                    radioButtonDividir = findViewById(R.id.radioButtonDividir);
+
+
+        radioButtonSuma.setOnClickListener(this);
+        radioButtonRestar.setOnClickListener(this);
+        radioButtonMultiplicar.setOnClickListener(this);
+        radioButtonDividir.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        EditText editTextPrimerNumero = findViewById(R.id.editTextPrimerNumero),
+                 editTextSegundoNumero = findViewById(R.id.editTextSegundoNumero);
+
+        TextView textResultado = findViewById(R.id.textViewResultado);
+
+        int primerNumero = 0,segundoNumero = 0,resultado = 0;
+
+        if(!editTextPrimerNumero.getText().toString().equals("") &&
+                !editTextSegundoNumero.getText().toString().equals("")
+        ) {
+            primerNumero = Integer.parseInt(editTextPrimerNumero.getText().toString());
+            segundoNumero = Integer.parseInt(editTextSegundoNumero.getText().toString());
+
+            switch (view.getId()) {
+
+                case R.id.radioButtonSumar:
+                    resultado = primerNumero + segundoNumero;
+                break;
+
+                case R.id.radioButtonRestar:
+                    resultado = primerNumero - segundoNumero;
+                break;
+
+                case R.id.radioButtonMultiplicar:
+                    resultado = primerNumero * segundoNumero;
+                break;
+
+                case R.id.radioButtonDividir:
+                    if(segundoNumero != 0){
+                        resultado = primerNumero / segundoNumero;
+                    }
+                break;
+
+            }
+        }
+
+        textResultado.setText(String.valueOf(resultado));
+    }
+
 }
