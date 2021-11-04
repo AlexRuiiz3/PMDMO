145b3cc2a6740ea9b671569adfe51add2f83a6f4
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-11-04 11:51:21 +0100

    Examen4Nov

---
+++ ExamenNov/app/src/main/java/com/example/examennov/Bike.java
@@ -0,0 +1,30 @@
+package com.example.examennov;
+
+public class Bike {
+
+    private String bikeName;
+    private int bikeImage;
+
+    public Bike(String bikeName, int bikeImage) {
+        this.bikeName = bikeName;
+        this.bikeImage = bikeImage;
+    }
+
+    public String getBikeName() {
+        return bikeName;
+    }
+
+    public void setBikeName(String bikeName) {
+        this.bikeName = bikeName;
+    }
+
+    public int getBikeImage() {
+        return bikeImage;
+    }
+
+    public void setBikeImage(int bikeImage) {
+        this.bikeImage = bikeImage;
+    }
+}
+
+
